d14482bb5a38d3642b71b4c32f6964640186c31f
IncludedGem <milesahuang@gmail.com>
2026-02-25 11:54:47 -0800

    photo carousel!

--- script.js
+++ script.js
@@ -6,3 +6,22 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
       .scrollIntoView({ behavior: 'smooth' });
   });
 });
+
+
+
+document.querySelectorAll('.carousel').forEach(carousel => {
+  const img = carousel.querySelector('img');
+  const projectFolder = carousel.dataset.project;
+  const totalImages = parseInt(carousel.dataset.count);
+  let current = 1;
+
+  carousel.querySelector('.left').addEventListener('click', () => {
+    current = current === 1 ? totalImages : current - 1;
+    img.src = `Project Photos/${projectFolder}/${current}.jpg`;
+  });
+
+  carousel.querySelector('.right').addEventListener('click', () => {
+    current = current === totalImages ? 1 : current + 1;
+    img.src = `Project Photos/${projectFolder}/${current}.jpg`;
+  });
+});
